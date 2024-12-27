ca424e43e102de3a97481993468498de31ea5b95
Neha00011 <112271756+Neha00011@users.noreply.github.com>
2024-12-27 17:32:02 +0530

    Add feature films section to Film component

--- portfolio_Website/src/pages/Film.tsx
+++ portfolio_Website/src/pages/Film.tsx
@@ -1,5 +1,12 @@
 function Film() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h3>Feature Films</h3>
+        <p>kxjhioeforlk</p>
+      </div>
+    </div>
+  );
 }
 
 export default Film;
